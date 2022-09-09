1b4e7c92ef3fb0d777f232650f18c1a98e882bab
bassoft1 <bassoft1@gmail.com>
2022-09-09 16:30:46 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
  *
  * always return 0 (success)
  */
-int main (void)
-{puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
