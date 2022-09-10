0eb732f053920c209d4ffc04a02b9babdc681274
bassoft1 <bassoft1@gmail.com>
2022-09-09 23:55:18 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,17 +4,20 @@
  *
  * Return: 0(successfully)
  */
+
 int main(void)
 {int a;
 	long int b;
 	long long int c;
 	char d;
 	float f;
-printf("size of a char:%lu byte(s)\n", (unsigned long)sizeof(d));
+
+	printf("size of a char:%lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of an in: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
 return (0);
 }
 
