a2f7d4d25f3fe33f0722dd993d5e346eb22f2f9f
bassoft1 <bassoft1@gmail.com>
2022-09-13 14:56:14 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - Checks if a character is lowercase.
-  @c: The character to be checked.
+ * @c:  is the int that will use for the argument of the function
  *
   * Return: 1 if character is lowercase, 0 otherwise.
   */
