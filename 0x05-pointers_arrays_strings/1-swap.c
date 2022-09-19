ec4eb4410b4d9639d7b7e426102ae74373f6eed2
bassoft1 <bassoft1@gmail.com>
2022-09-19 15:55:41 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*swap_int - swaps two integers' values
+*@a: first integer
+*@a: first integer
+*Return: 0
+*/
+
+void swap_int(int *a, int *b)
+{
+int tmp = *a;
+*a = *b;
+*b = tmp;
+}
+
+
