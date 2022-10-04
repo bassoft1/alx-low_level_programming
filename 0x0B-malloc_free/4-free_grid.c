953fb6a86b410c0b2ee152ff2338806af0bd19cd
bassoft1 <bassoft1@gmail.com>
2022-10-03 22:49:51 -0700

    tadk4-6

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,58 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include main.h
 /**
- * _strlen - length of a string
- * @s: the string
- * Return: length
- **/
-int _strlen(char *s)
-{
-	int len;
-
-	if (!s)
-		return (0);
-	for (len = 0; s[len]; len++)
-	{
-	}
-	return (len);
-}
-/**
- * argstostr - concasts args to str
- * @ac: # of args
- * @av: names of args
- * Return: char *
+ * free_grid - free a 2d grid
+ * @grid: the grid
+ * @height: the height
+ * Return: void
  */
-char *argstostr(int ac, char **av)
+void free_grid(int **grid, int height)
 {
 	int i;
-	int j;
-	int x = 0;
-	int len = 0;
-	char *newstring;
 
-	if (!ac || !av)
+	for (i = 0; i < height; i++)
 	{
-		return (NULL);
+		free(grid[i]);
 	}
-	for (i = 0; i < ac; i++)
-	{
-		len += _strlen(*(av + i));
-	}
-	newstring = malloc(sizeof(char) * (len + ac + 1));
-	if (!newstring)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++, x++)
-		{
-			newstring[x] = av[i][j];
-		}
-		newstring[x++] = '\n';
-	}
-	newstring[x] = '\0';
-	return (newstring);
+	free(grid);
 }
 
